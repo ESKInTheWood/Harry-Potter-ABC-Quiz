7cdfdd55f960bd0c56a395687728a5cc2c33c53b
Emilia S-K <e.s-k@wp.pl>
2024-03-27 10:24:16 +0100

    Question class

---
+++ src/main/java/org/example/Question.java
@@ -0,0 +1,29 @@
+package org.example;
+
+public class Question {
+    private String pytanie;
+    private String a;
+    private String b;
+    private String c;
+    private String prawidłowaOdpowiedź;
+
+    public String getPytanie() {
+        return pytanie;
+    }
+
+    public String getA() {
+        return a;
+    }
+
+    public String getB() {
+        return b;
+    }
+
+    public String getC() {
+        return c;
+    }
+
+    public String getPrawidłowaOdpowiedź() {
+        return prawidłowaOdpowiedź;
+    }
+}
